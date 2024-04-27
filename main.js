924ecf4c3517f0642a1c18f15f7d5c93f1c197cb
FilippoBonanno <fbonanno15@gmail.com>
2024-04-27 14:07:03 +0200

    M-2

--- main.js
+++ main.js
@@ -14,10 +14,20 @@ let list = [
 console.log(list);
 
 
-// CREO UN CICLO FOR CHE MI ITERA OGNI ELEMENTO PARTENDO DALLA POSIZIONE 0 FINO A FINE LISTA
-for (let i = 0; i < list.length; i++) {
-    // CREO UNA CONSTANTE "ELEMENT" CHE SIA = ALL'ELEMENTO IN QUEL MOMENTO IN LISTA PIù i 
-	const element = list[i];
-	console.log(i, element);
+// // CREO UN CICLO FOR CHE MI ITERA OGNI ELEMENTO PARTENDO DALLA POSIZIONE 0 FINO A FINE LISTA
+// for (let i = 0; i < list.length; i++) {
+//     // CREO UNA CONSTANTE "ELEMENT" CHE SIA = ALL'ELEMENTO IN QUEL MOMENTO IN LISTA PIù i 
+// 	const element = list[i];
+// 	console.log(i, element);
 	
+// }
+let i = 0
+while (i < list.length) {
+    
+
+    const element = list[i];
+    console.log(element);
+
+    i++;
+
 }
